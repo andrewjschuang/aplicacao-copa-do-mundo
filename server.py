b63aea1aa6eed1bcc28ebd449854becfb801dd30
Andrew Chuang <andrewjschuang@gmail.com>
2018-05-17 15:57:03 -0300

    Add form

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from database import *
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/consulta')
-def consulta():
-    return 'Pagina de consulta'
+conn, cursor = init()
 
-@app.route('/resultado')
-def resultado():
-    return 'Resultado da consulta'
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/consulta', methods=['POST', 'GET'])
+def consulta(last_selected=None):
+    if request.method == 'POST':
+        query = request.form.get('query')
+        # results = select(cursor, query)
+        results = query
+        return render_template('form.html', results=results)
+    return render_template('form.html')
+
 
 app.run(debug=True, use_reloader=True)
