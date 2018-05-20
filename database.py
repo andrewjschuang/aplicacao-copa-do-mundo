cd50469e907b645775200cf2fb476e59c225850c
João Phillipe Cardenuto <phillipe.cardenuto@gmail.com>
2018-05-20 15:31:31 -0300

    Código server.py e database.py comentados

--- database.py
+++ database.py
@@ -9,10 +9,10 @@ class Connection(object):
             self.cursor = self.conn.cursor()
         except:
             print("Não foi possível se conectar ao Banco de Dados")
-            raise 
+            raise
+        # Variables used in more than a method
         self.authorized = 0
         self.id_user = None
-        
         # result = select(cursor, 'cities')
         # printResults(result)
 
@@ -24,35 +24,38 @@ class Connection(object):
         if condition:
             pass
         else:
-            #command = ('SELECT * FROM torcedor \
-            #            WHERE email=%sand senha=%s' % (email,password))
+            #Check if email with the right password is valid
             command = ('SELECT nomepessoa FROM pessoa NATURAL JOIN torcedor WHERE email=%s AND senha=%s' % (email,password))
             self.cursor.execute(command)
             result = self.cursor.fetchall()
             print(result)
             return result
-
+    # Try to register a new user, if a new email is given
     def registerSucess(self,name,password,email,nationality,condition=None):
         if condition:
             pass
         else:
+            #IF email exists , return False
             command = ('SELECT email FROM  torcedor WHERE email=%s' % (email))
             self.cursor.execute(command)
             result = self.cursor.fetchall()
             print(result)
             if result != []:
                 return False
-
+            # If email not registered, choose a new idpessoa for the new user based on the existing idpessoa 
             command = ('SELECT max(idpessoa) FROM  pessoa' )
             self.cursor.execute(command)
             result = self.cursor.fetchall()
             idpessoa_new = result[0][0] + 1
+            # Insert new person on pessoa
             command = ('INSERT INTO pessoa (idpessoa,nomepessoa,nacionalidade) VALUES (%s,%s,%s);' % (idpessoa_new,name,nationality))
             self.cursor.execute(command)
+            # Commit the DB write
             self.conn.commit()
-            
+            #Insert new pessoa in torcedor
             command = ('INSERT INTO torcedor (idpessoa,email,senha) VALUES (%s,%s,%s);' % (idpessoa_new,email,password))
             self.cursor.execute(command)
+            # Commit the DB write
             self.conn.commit()
             return True
 
@@ -61,8 +64,9 @@ class Connection(object):
         if condition:
             pass
         else:
+            # For a pretty table html, make the querry with pandas api, then send result to server
             result = psql.read_sql('SELECT * FROM %s' % relation,self.conn)
-           
+            # OLD Version of querry without pretty tables
             """self.cursor.execute(command)
             return self.cursor.declare(),self.cursor.fetchall()"""
             return result
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import database
 app = Flask(__name__)
 app.static_folder = "static"
 connection = database.Connection(dbname='mydb', user='phillipe')
-# Insert a " for the querry
+# Insert a " in the begin and end of querry
 def putsQuot(line):
 	return "\'"+line+"\'"
 
@@ -15,28 +15,31 @@ def putsQuot(line):
 # Login With a email and password
 def login():
 	if request.method == 'POST' and "login" in request.form :  #this block is only entered when the form is submitted
-
+		# Reques user email and password for check registers in DB
 	    email = request.form.get('user_email')
 	    password = request.form.get('password')
 	    email="\'"+email+"\'"
 	    password="\'"+password+"\'"
 	    results = connection.login_user(email,password)
-
+	    #IF Regsiter not found return a HTML with ACESS DENIED
 	    if(results == []):
 	        return '''<h1>ACESS DENIED</h1>
 	        		  <h1>EMAIL AND/OR PASSWORD INCORRECT</h1>'''
 
 	    #return '''<h1>USERNAME: {}</h1>'''.format(results[0][0])
+	    # IF Register found goes to  page consulta
 	    return redirect(url_for('consulta'))
-
+	# IF botton register was click goes to page registra
 	elif request.method == 'POST' and  'register' in request.form:
 		 return redirect(url_for('registra'))
 
 	else:
+		# If nothing was actioned render the index.html
 	    return render_template('index.html')
-	    
+# Page registra
 @app.route('/registra',methods=['POST','GET'])
 def registra():
+	# if all field of forms was filled querry try to insert new user in DB
 	if request.method == 'POST' and "submit" in request.form:
 		name = request.form.get('name')
 		nationality = request.form.get('nationality')
@@ -47,24 +50,25 @@ def registra():
 		password= putsQuot(password)
 		nationality = putsQuot(nationality)
 		name = putsQuot(name)
-
+		# IF email not in BD register new user , then goes to page login
 		if(connection.registerSucess(name,password,email,nationality)):
 			return redirect(url_for('login'))
 		else:
+			# Else print EMAIL ALREADY REGISTERED in the header of the page
 			return render_template(('form-register.html'),already_exists="EMAIL ALREADY REGISTERED")
 
-
+	# If nothing was actioned render the form-register.html
 	return render_template('form-register.html')
-
+# Page consulta
 @app.route('/consulta', methods=['POST', 'GET'])
 def consulta():
-
+	# Make a querry in DB
 	if request.method == 'POST':
 	    query = request.form.get('query')
 	    df = connection.select(query)
-
-	    
+	    # Return result Table render in html
 	    return render_template('form.html',name='RESULTADO',data = df.to_html())
+	# If nothing was actioned render the form.html
 	return render_template('form.html',)
-
+# Run app
 app.run(debug=True, use_reloader=True)
