25337b4a9683a0e07aac4098b9c48fdd87cf6b17
João Phillipe Cardenuto <phillipe.cardenuto@gmail.com>
2018-05-22 11:52:04 -0300

    Alterar as informações de cadastro do próprio usuário (FEITO)

--- database.py
+++ database.py
@@ -11,8 +11,12 @@ class Connection(object):
             print("Não foi possível se conectar ao Banco de Dados")
             raise
         # Variables used in more than a method
-        self.authorized = 0
+        self.authorized = False
         self.id_user = None
+        self.email_user = None
+        self.password_user = None
+        self.name_user = None
+        self.nationality = None
         # result = select(cursor, 'cities')
         # printResults(result)
 
@@ -24,12 +28,29 @@ class Connection(object):
         if condition:
             pass
         else:
-            #Check if email with the right password is valid
-            command = ('SELECT nomepessoa FROM pessoa NATURAL JOIN torcedor WHERE email=%s AND senha=%s' % (email,password))
-            self.cursor.execute(command)
-            result = self.cursor.fetchall()
-            print(result)
-            return result
+        	#Check if email with the right password is valid
+        	result = []
+        	command = ('SELECT idpessoa,nomepessoa,nacionalidade FROM pessoa NATURAL JOIN torcedor WHERE email=%s AND senha=%s' % (email,password))
+        	self.cursor.execute(command)
+        	result = self.cursor.fetchall()
+        	print(result)
+        	# USER INFORMATION INSTANCE
+        	if result != []:
+        		self.authorized = True
+        		self.id_user = result[0][0]
+        		self.name_user = result[0][1]
+        		self.nationality = result[0][2]
+        		self.email_user = email[1:-1] #remove quots
+        		self.password_user = password[1:-1] #remove quots
+
+        	else:
+        		self.authorized = False
+        		self.id_user = None
+        		self.name_user = None
+        		self.nationality = None
+        		self.email_user = None
+        		self.password_user = None
+        	return result
     # Try to register a new user, if a new email is given
     def registerSucess(self,name,password,email,nationality,condition=None):
         if condition:
@@ -57,6 +78,11 @@ class Connection(object):
             self.cursor.execute(command)
             # Commit the DB write
             self.conn.commit()
+            #Update instance
+            self.name_user = name[1:-1] #remove quots
+            self.nationality = nationality[1:-1] #remove quots
+            self.email_user = email[1:-1] #remove quots
+            self.password_user = password[1:-1] #remove quots
             return True
 
 
@@ -70,6 +96,19 @@ class Connection(object):
             """self.cursor.execute(command)
             return self.cursor.declare(),self.cursor.fetchall()"""
             return result
+
+    def update_settings(self,name,nationality,email,password,condition =None):
+    	if condition:
+    		pass
+    	else:
+    		command = ('UPDATE pessoa SET nomepessoa=%s , nacionalidade=%s WHERE idpessoa=%s ;' % (name,nationality,self.id_user))
+    		self.cursor.execute(command)
+    		command = ('UPDATE torcedor SET email=%s , senha=%s WHERE idpessoa=%s ;' % (email,password,self.id_user))
+    		self.cursor.execute(command)
+    		self.conn.commit()
+    		return True
+
+
 
     def printResults(self,results):
         for item in results:
--- server.py
+++ server.py
@@ -14,6 +14,7 @@ def putsQuot(line):
 @app.route("/index.html", methods=['POST','GET'])
 # Login With a email and password
 def login():
+	connection.id_user = None #Inicialize id_user in login page
 	if request.method == 'POST' and "login" in request.form :  #this block is only entered when the form is submitted
 		# Reques user email and password for check registers in DB
 	    email = request.form.get('user_email')
@@ -70,5 +71,53 @@ def consulta():
 	    return render_template('form.html',name='RESULTADO',data = df.to_html())
 	# If nothing was actioned render the form.html
 	return render_template('form.html',)
+
+# Page configuracao
+@app.route('/configuracao', methods=['POST', 'GET'])
+def configuracao():
+	if connection.id_user == None:
+		#Login Settings
+		if request.method == 'POST' and "login" in request.form :  #this block is only entered when the form is submitted
+			# Reques user email and password for check registers in DB
+		    email = putsQuot(request.form.get('user_email'))
+		    password = putsQuot(request.form.get('password'))
+		    results = connection.login_user(email,password)
+		    #IF Regsiter not found return a HTML with ACESS DENIED
+		    if(results == []):
+		        return '''<h1>ACESS DENIED</h1>
+		        		  <h1>EMAIL AND/OR PASSWORD INCORRECT</h1>'''
+		    # If User Found goes to Settings
+		    else:
+		    	return render_template('configuracao.html',username=connection.name_user,nationality=connection.nationality,email=connection.email_user,password=connection.password_user)
+		return render_template('login_update.html')
+
+	# Make a update_settings in DB with idpessoa as id_user
+	if request.method == 'POST' and 'update' in request.form:
+		blank_field = 0
+		name = putsQuot(request.form.get('name'))
+		if name[1:-1] == "": # name without quots
+			blank_field = blank_field +1
+			name = putsQuot(connection.name_user)
+		nationality = putsQuot(request.form.get('nationality'))
+		if nationality[1:-1] == "": # nationality without quots
+			blank_field = blank_field +1
+			nationality = putsQuot(connection.nationality)
+		email = putsQuot(request.form.get('email'))
+		if email[1:-1] == "": # email without quots
+			blank_field = blank_field +1
+			email = putsQuot(connection.email_user)
+		password = putsQuot(request.form.get('password'))
+		if password[1:-1] == "": # password without quots
+			blank_field = blank_field +1
+			password = putsQuot(connection.password_user)
+
+		if(blank_field<4): # IF USER TYPE IN SOME FIELD
+			if(connection.update_settings(name,nationality,email,password)):
+				return redirect(url_for('login'))
+	  
+	# If nothing was actioned render the form.
+	print(connection.nationality)
+	return render_template('configuracao.html',username=connection.name_user,nationality=connection.nationality,email=connection.email_user,password=connection.password_user)
+
 # Run app
 app.run(debug=True, use_reloader=True)
